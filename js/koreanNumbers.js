ce2c92854c91ec3328de0cb205a78a5ce6293e9a
Gabrielle Harang <gabrielle.harang@etu.unicaen.fr>
2023-09-28 15:38:16 +0200

    :lipstick:

--- js/koreanNumbers.js
+++ js/koreanNumbers.js
@@ -1,11 +1,24 @@
+const sinoKorean = ['공', '일', '이', '삼', '사', '오', '육', '칠', '팔', '구', '십'];
+
 const nativeKorean = ['공', '하나', '둘', '셋', '넷', '다섯',
  '여삿', '일곱', '여덟', '아홉', '열'];
 
-let theCard = document.getElementById("card");
 let number = getRandomNumber(0, 10);
-theCard.innerHTML += "The number :" + number + "<br>";
-theCard.innerHTML += "In native korean :" + nativeKorean[number];
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max+1-min) + min);
 }
+
+
+var cards = document.querySelectorAll('.card');
+
+theCardNumber = document.getElementsByClassName("card__face--front")[0];
+theCardNumber.innerHTML = number;
+theCardSinoKorean = document.getElementsByClassName("card__face--back")[0];
+theCardSinoKorean.innerHTML = sinoKorean[number];
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
