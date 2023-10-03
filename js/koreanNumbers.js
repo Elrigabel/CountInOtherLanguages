3e74938601425564db0919dcb3aab6f5a9b80314
Gabrielle Harang <gabrielle.harang@etu.unicaen.fr>
2023-10-03 23:11:55 +0200

    :sparkles:

--- js/koreanNumbers.js
+++ js/koreanNumbers.js
@@ -5,17 +5,40 @@ const nativeKorean = ['공', '하나', '둘', '셋', '넷', '다섯',
 
 let number = getRandomNumber(0, 10);
 
+var cards = document.querySelectorAll('.card');
+let nextNumberButton = document.getElementById("nextNumber");
+let theCardNumber = document.getElementsByClassName("card__face--front")[0];
+let theCardSinoKorean = document.getElementsByClassName("card__face--back")[0];
+
+
+nextNumberButton.addEventListener("click", () => {
+  number = getRandomNumber(0, 99);
+  console.log(number);
+  theCardNumber.innerHTML = number;
+  theCardSinoKorean.innerHTML = getStringFromNumber(number);
+});
+
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max+1-min) + min);
+  return Math.floor(Math.random() * (max+1-min) + min);
 }
 
+function getStringFromNumber(n) {
+  let inSinoKorean = '';
+  if(n > 10) {
+    inSinoKorean += sinoKorean[Math.floor(n / 10)] + '십';
+    console.log(Math.floor(n / 10));
+    inSinoKorean += sinoKorean[n%10];
+    console.log(n%10);
+  }
+  else {
+    inSinoKorean = sinoKorean[n]
+  }
+  
+  console.log(inSinoKorean);
+  return inSinoKorean;
+}
 
-var cards = document.querySelectorAll('.card');
-
-theCardNumber = document.getElementsByClassName("card__face--front")[0];
-theCardNumber.innerHTML = number;
-theCardSinoKorean = document.getElementsByClassName("card__face--back")[0];
-theCardSinoKorean.innerHTML = sinoKorean[number];
+/* --------- Flip the card ----------*/
 
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
